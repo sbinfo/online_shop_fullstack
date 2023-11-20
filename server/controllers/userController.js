2e0f1b58e0034533725df8a1f73a3ff8811842d2
Said Babaiev <babaiev.info@gmail.com>
2023-11-20 13:34:29 +0100

    made error handler and made ErrorHanlingMiddleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,20 @@
+const ApiError = require("../error/ApiError")
+
 class UserController {
     async registration (req, res) {
-
+        
     }
 
     async login (req, res) {
-
+        
     }
 
-    async check (req, res) {
+    async check (req, res, next) {
         const { id } = req.query
-        res.json(id)
+        if (!id) {
+            return next(ApiError.badRequest('User ID not specified.'))
+        }
+        res.json(`user id: ${ id }`)
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000
 
@@ -11,6 +12,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+/*
+  Middleware которое работает с ошибками обязательно должен идти в конце
+  Так как он замыкаюший, если произошла ошибка мы прекрашаем работу и возврашаем ошибку
+*/
+app.use(errorHandler) 
 
 
 const start = async () => {
