df69315e2122bcc18bd2374ee5940b2059e28e62
Said Babaiev <babaiev.info@gmail.com>
2023-11-22 18:04:17 +0100

    made jwt autorization, registration and login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,65 @@
-const ApiError = require("../error/ApiError")
+const ApiError = require("../error/ApiError");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { User, Basket } = require('../models/models');
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        { id, email, role },
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    );
+}
 
 class UserController {
-    async registration (req, res) {
-        
+    async registration (req, res, next) {
+        const { email, password, role } = req.body;
+
+        if (!email) {
+            next(ApiError.badRequest("Incorrect email"));
+        }
+
+        if (!password) {
+            next(ApiError.badRequest("Incorrect password"));
+        }
+
+        const candidate = await User.findOne({ where: {email} });
+        if (candidate) {
+            next(ApiError.badRequest("A user with this email already exists"));
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({ email, role, password: hashPassword });
+        const basket = await Basket.create({ userId: user.id });
+        const token = generateJwt(user.id, user.email, user.role)
+
+       res.json({ token }) 
     }
 
-    async login (req, res) {
-        
+    async login (req, res, next) {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({ where: {email} });
+        if (!user) {
+            return next(ApiError.internal("User with this email not found"));
+        }
+
+        let comparePassword = bcrypt.compareSync(password, user.password);
+        if (!comparePassword) {
+            return next(ApiError.internal("Incorrect password"));
+        }
+
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json({ token });
     }
 
     async check (req, res, next) {
-        const { id } = req.query
+        const { id } = req.query;
         if (!id) {
-            return next(ApiError.badRequest('User ID not specified.'))
+            return next(ApiError.badRequest('User ID not specified.'));
         }
-        res.json(`user id: ${ id }`)
+        res.json(`user id: ${ id }`);
     }
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
