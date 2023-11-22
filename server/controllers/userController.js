98d3d9940bf7e5819ca621af6544844c6d64f3b4
Said Babaiev <babaiev.info@gmail.com>
2023-11-22 18:20:32 +0100

    made check authorization

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -54,11 +54,8 @@ class UserController {
     }
 
     async check (req, res, next) {
-        const { id } = req.query;
-        if (!id) {
-            return next(ApiError.badRequest('User ID not specified.'));
-        }
-        res.json(`user id: ${ id }`);
+        const token = generateJwt(req.user.id, req.user.email, req.user.role);
+        return res.json({token})
     }
 }
 
